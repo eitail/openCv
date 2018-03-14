2a8c8c27e8e95370207d3c4900da81ca8afaebeb
leo <eitail@163.com>
2018-03-14 10:25:18 -0700

    add function flip(),erode(),dilate(),morphologyEx()

---
+++ mainwindow.cpp
@@ -0,0 +1,203 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    size    = 6;
+    size2   = 3;
+
+    //menuInit();
+    QLabel_Pic_Init();
+    LoadImg();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
+void MainWindow::menuInit()
+{
+    qDebug() << "menuInit";
+    status = new QStatusBar(this);
+    menu[0] = new QMenu("File");
+    menu[0]->addAction("Edit");
+    menu[0]->addAction("Check");
+    menu[0]->addAction("Tool");
+
+     act[0] = new QAction("Create",this);
+     act[0]->setShortcut(Qt::CTRL | Qt::Key_A );
+     act[0]->setStatusTip("new ");
+
+     act[1] = new QAction("open",this);
+     act[1]->setCheckable(true);
+
+    menu[1] = new QMenu("save");
+    menu[1]->addAction(act[0]);
+    menu[1]->addAction(act[1]);
+
+    menu[2] = new QMenu("print");
+    menu[2]->addAction("set ");
+    menu[2]->addMenu(menu[1]);
+
+    menuBar = new QMenuBar(this);
+    menuBar->addMenu(menu[0]);
+    menuBar->addMenu(menu[2]);
+    menuBar->setGeometry(0,100,200,30);
+
+    connect(menuBar,SIGNAL(triggered(QAction*)),this,SLOT(trigerMenu(QAction*)));
+
+}
+
+void MainWindow::trigerMenu(QAction* act)
+{
+    if(act->text() == "Create")
+    {
+        qDebug()<<"新建菜单被按下";
+    }
+}
+
+//--------------------------------------------------------------------
+
+void MainWindow::LoadImg()
+{
+    srcImage = cv::imread("/root/Demo_opencv/openCv/pic/3.jpg");
+    if(!srcImage.data)
+    {
+           qDebug() << "Load img error!";
+    }
+    else
+    {
+        cv::cvtColor(srcImage,srcImage,CV_BGR2RGB);
+        img = QImage((const unsigned char*)(srcImage.data),srcImage.cols,srcImage.rows,srcImage.cols*srcImage.channels(),QImage::Format_RGB888);
+        ui->QL_dst->clear();
+        img = img.scaled(ui->QL_src->width(),ui->QL_src->height());
+        ui->QL_src->setPixmap(QPixmap::fromImage(img));
+    }
+
+}
+
+void MainWindow::QLabel_Pic_Init()
+{
+    ui->QL_src->setGeometry(10,60,300,300);
+    ui->QL_dst->setGeometry(10,380,300,300);
+}
+
+void MainWindow::putQlabel()
+{
+    img = QImage((const unsigned char*)(dstImage.data),dstImage.cols,dstImage.rows,dstImage.cols*dstImage.channels(),QImage::Format_RGB888);
+    ui->QL_dst->clear();
+    img = img.scaled(ui->QL_dst->width(),ui->QL_dst->height());
+    ui->QL_dst->setPixmap(QPixmap::fromImage(img));
+}
+
+//--水平翻转--
+void MainWindow::on_flip_horizontal_clicked()
+{
+    cv::flip(srcImage,dstImage,1);
+    putQlabel();
+}
+
+//--垂直翻转--
+void MainWindow::on_flip_vertical_clicked()
+{
+    cv::flip(srcImage,dstImage,0);
+    putQlabel();
+}
+
+//--水平垂直翻转--
+void MainWindow::on_flip_hor_and_ver_clicked()
+{
+    cv::flip(srcImage,dstImage,-1);
+    putQlabel();
+}
+
+//--腐蚀--
+void MainWindow::on_form_corrosion_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size, size), Point(-1, -1));
+    erode(srcImage, dstImage, structureElement);
+    putQlabel();
+}
+
+//--膨胀--
+void MainWindow::on_form_swell_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2, size2), Point(-1, -1));
+    dilate(srcImage, dstImage, structureElement);
+    putQlabel();
+}
+
+//--开运算--
+void MainWindow::on_form_open_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2,size2), Point(-1, -1));
+    morphologyEx(srcImage, dstImage, CV_MOP_OPEN, structureElement);
+    putQlabel();
+}
+
+//--闭运算--
+void MainWindow::on_form_close_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2, size2), Point(-1, -1));
+    morphologyEx(srcImage, dstImage, CV_MOP_CLOSE, structureElement);
+    putQlabel();
+}
+
+//--形态学梯度--
+void MainWindow::on_form_gradient_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2, size2), Point(-1, -1));
+    morphologyEx(srcImage, dstImage, CV_MOP_GRADIENT, structureElement);
+    putQlabel();
+}
+
+//--顶帽--
+void MainWindow::on_form_top_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2, size2), Point(-1, -1));
+    morphologyEx(srcImage, dstImage, CV_MOP_TOPHAT, structureElement);
+    putQlabel();
+}
+
+//--黑帽--
+void MainWindow::on_form_black_clicked()
+{
+    Mat structureElement = getStructuringElement(MORPH_RECT, Size(size2, size2), Point(-1, -1));
+    morphologyEx(srcImage, dstImage, CV_MOP_BLACKHAT, structureElement);
+    putQlabel();
+}
+
+//--均值滤波--
+void MainWindow::on_filter_aver_clicked()
+{
+
+}
+
+//--方框滤波--
+void MainWindow::on_filter_box_clicked()
+{
+
+}
+
+//--高斯滤波--
+void MainWindow::on_filter_gaussian_clicked()
+{
+
+}
+
+//--中值滤波--
+void MainWindow::on_filter_median_clicked()
+{
+
+}
+
+//--双边滤波--
+void MainWindow::on_filter_bilateral_clicked()
+{
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,75 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QDebug>
+
+//#include <opencv.hpp>
+ #include <opencv2/opencv.hpp>
+//#include <opencv2/core/core.hpp>
+
+
+
+
+using namespace std;
+using namespace cv;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    QMenu*          menu[10];
+    QAction*        act[10];
+    QMenuBar*       menuBar;
+    QStatusBar*     status;
+
+    //-----------------------
+    cv::Mat     srcImage;
+    cv::Mat     dstImage;
+    QImage      img;
+    int         size;
+    int         size2;
+
+private:
+    void menuInit();
+    void LoadImg();
+    void QLabel_Pic_Init();
+    void putQlabel();
+
+private:
+    Ui::MainWindow *ui;
+
+public slots:
+    void trigerMenu(QAction* act);
+
+private slots:
+    void on_flip_horizontal_clicked();
+    void on_flip_vertical_clicked();
+    void on_flip_hor_and_ver_clicked();
+    void on_form_corrosion_clicked();
+    void on_form_swell_clicked();
+    void on_form_open_clicked();
+    void on_form_close_clicked();
+    void on_form_gradient_clicked();
+    void on_form_top_clicked();
+    void on_form_black_clicked();
+    void on_filter_aver_clicked();
+    void on_filter_box_clicked();
+    void on_filter_gaussian_clicked();
+    void on_filter_median_clicked();
+    void on_filter_bilateral_clicked();
+};
+
+#endif // MAINWINDOW_H
